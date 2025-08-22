ee833cb4aaa1493834f5af25f4da02dbf003a58e
Shahan Khan <shahan.ali.khan.fiem@gmail.com>
2025-08-22 20:36:38 +0530

    removed everything

--- src/app/blog/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function BlogPage() {
-  return (
-    <section className="min-h-screen bg-gray-950 text-gray-200 p-10">
-      <h2 className="text-4xl font-bold mb-6">Blog</h2>
-      <p className="text-lg">Coming soon... ✍️</p>
-    </section>
-  );
-}
